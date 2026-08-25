063547ba97fadd1f9e24d5269ddac1c0d89268ae
davidhalla10 <davidhalla10@gmail.com>
2026-08-25 19:38:52 -0400

    Basic layout for HomePage is set.

--- unit-1-project/src/components/pages/HomePage.jsx
+++ unit-1-project/src/components/pages/HomePage.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 function HomePage () {
     return(
-        <>
+        <>  
+        <Header />
         <main>
-            <Header />
             <div>
                 <h2>Welcome To True Carry!</h2>
                 <p>The app to get your true distance.</p>
+                <button>Get Your Distances</button> 
             </div>
         </main>
+        <Footer />
         </>
     )
 }
