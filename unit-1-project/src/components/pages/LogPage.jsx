a69fc5fab49eadc4b39659c23c1e91f88d7d13cc
davidhalla10 <davidhalla10@gmail.com>
2026-09-01 00:11:19 -0400

    Added a function to handle calculation

--- unit-1-project/src/components/pages/LogPage.jsx
+++ unit-1-project/src/components/pages/LogPage.jsx
@@ -14,7 +14,7 @@ const targets = [50, 75, 100, 125, 150, 175, 200, 225];
 function LogPage () {
 
     const [target, setTarget] = useState(null);
-    const [step, setStep] = useState('target');
+    const [step, setStep] = useState('results');
     const [shots, setShots] = useState([]);
 
     function pickTarget (yards) {
@@ -29,6 +29,10 @@ function LogPage () {
             yards: target + outcome.offset,
         }
         setShots([...shots, newShot]);
+    }
+
+    function calculate () {
+        setStep("results");
     }
 
     if (step === "target") {
