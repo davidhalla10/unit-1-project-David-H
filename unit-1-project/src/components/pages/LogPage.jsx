2ec7e23fd11fca96db8a7b675a0dfc639ae60e58
davidhalla10 <davidhalla10@gmail.com>
2026-08-31 22:06:03 -0400

    Added .map to map a button to each yardage from targets

--- unit-1-project/src/components/pages/LogPage.jsx
+++ unit-1-project/src/components/pages/LogPage.jsx
@@ -3,18 +3,29 @@ import { useState } from "react";
 
 const targets = [50, 75, 100, 125, 150, 175, 200, 225];
 
-function LogPage() {
+function LogPage () {
 
     const [target, setTarget] = useState(null);
     const [step, setStep] = useState('target');
     const [shots, setShots] = useState([]);
+
+    function pickTarget (yards) {
+        setTarget(yards);
+        setStep("shots");
+    }
 
     return (
         <>
         <main>
             <h1>Select A Target</h1>
             <ul>
-                
+                {targets.map((yards) => (
+                    <li key={yards}>
+                        <button onClick={() => pickTarget(yards)}>
+                            <span>{yards} yds</span>
+                        </button>
+                    </li>
+                ))}
             </ul>
         </main>
         </>
