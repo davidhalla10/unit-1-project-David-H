2c1f53024d8eab9230ddf0a9c822703437032356
davidhalla10 <davidhalla10@gmail.com>
2026-08-31 21:57:04 -0400

    Created useStates for log page and created pickTarget function

--- unit-1-project/src/components/pages/LogPage.jsx
+++ unit-1-project/src/components/pages/LogPage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 const targets = [50, 75, 100, 125, 150, 175, 200, 225];
 
 function LogPage() {
 
     const [target, setTarget] = useState(null);
+    const [step, setStep] = useState('target');
+    const [shots, setShots] = useState([]);
 
     return (
         <>
         <main>
             <h1>Select A Target</h1>
-
-
+            <ul>
+                
+            </ul>
         </main>
         </>
     )
