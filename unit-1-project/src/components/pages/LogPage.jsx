7923b7175cf01bff0bbe6a603ab0e2903d42c487
davidhalla10 <davidhalla10@gmail.com>
2026-08-31 21:43:02 -0400

    Added targets array for flag distances

--- unit-1-project/src/components/pages/LogPage.jsx
+++ unit-1-project/src/components/pages/LogPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const targets = [50, 75, 100, 125, 150, 175, 200, 225];
+
 function LogPage() {
+
+    const [target, setTarget] = useState(null);
+
     return (
         <>
-        
+        <main>
+            <h1>Select A Target</h1>
+
+
+        </main>
         </>
     )
 }
